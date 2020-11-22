96518a16f7a36787861d2397a461dcd31aa3c8ff
Jordan Paz <jordanpaz@MacBook-Air.domain>
2020-11-21 21:35:44 -0800

    Art grid

--- components/layout.js
+++ components/layout.js
@@ -1,66 +1,75 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styled from 'styled-components';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = 'Jordan Paz'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Jordan Paz';
+export const siteTitle = 'Jordan Paz';
 
 export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
+    return (
+        <Container>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Jordan Paz is a front end engineer and artist."
                 />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>
-  )
+                <meta
+                    property="og:image"
+                    content={`https://og-image.now.sh/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <img
+                            src="/images/profile.jpg"
+                            className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                            alt={name}
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <a>
+                                <img
+                                    src="/images/profile.jpg"
+                                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                                    alt={name}
+                                />
+                            </a>
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>
+                                    {name}
+                                </a>
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    max-width: 46rem;
+    padding: 0 0.5rem;
+    margin: 3rem auto 6rem;
+`;
--- pages/art.js
+++ pages/art.js
@@ -1,9 +1,52 @@
+import _ from 'lodash';
+import Head from 'next/head';
 import styled from 'styled-components';
 
-import Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/layout';
 import { MainText } from '../components/typography';
 
 export default function Art() {
-    return <Layout><MainText>This is where my art will go</MainText></Layout>
+    return (
+        <Layout>
+            <Head>
+                <title>Jordan Paz | Art</title>
+            </Head>
+            <h1>Drawings</h1>
+            <Grid>
+                {_.times(15, () => (
+                    <ImageContainer>
+                        <FakeImage />
+                    </ImageContainer>
+                ))}
+            </Grid>
+        </Layout>
+    );
 }
- 
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(47%, 1fr));
+    @media (min-width: 576px) {
+        grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
+    }
+    grid-gap: 0.5rem;
+`;
+
+const ImageContainer = styled.div`
+    position: relative;
+    border: 1px solid;
+    box-sizing: border-box;
+    ::before {
+        content: '';
+        display: block;
+        padding-top: 100%;
+    }
+`;
+
+const FakeImage = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+`;
