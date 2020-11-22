2ed5525b6b342addeeca2824932fcc67b6454064
Jordan Paz <jordanpaz@MacBook-Air.domain>
2020-11-21 20:37:44 -0800

    Fix stuff

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import { MainText } from '../components/typography'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
     return (
@@ -26,11 +27,13 @@ export default function Home({ allPostsData }) {
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
