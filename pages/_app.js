6b176136281400f802625275ca8c020c0b0597cb
Jordan Paz <jordanpaz@Jordans-Air.domain>
2020-12-03 08:03:23 -0800

    Add apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
 import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apolloClient';
 import { light, dark } from 'styles/theme';
 import ThemeContext from 'theme-context';
 import GlobalStyles from 'styles/global-styles';
 
 export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
   const [theme, setTheme] = React.useState(light);
 
   const toggleTheme = () => setTheme(theme === light ? dark : light);
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </ApolloProvider>
   );
 }
